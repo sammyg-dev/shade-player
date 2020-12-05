8c5ae57b802656e557e6c7a6bed264343849205c
sammyg-dev <me@sammyg.dev>
2020-12-04 23:22:43 -0800

    revamped Layer as ShaderLayer

--- src/ShadePlayerApp.hpp
+++ src/ShadePlayerApp.hpp
@@ -1,8 +1,8 @@
 #ifndef _SHADEPLAYER_INCLUDE_SHADEPLAYERAPP_HPP_
 #define _SHADEPLAYER_INCLUDE_SHADEPLAYERAPP_HPP_
-
 #include <raylib.h>
-#include "LayerStack.hpp"
+//#include "LayerStack.hpp"
+#include "ShaderLayer.hpp"
 
 #if defined(PLATFORM_DESKTOP)
     #define GLSL_VERSION            330
@@ -22,22 +22,16 @@ namespace shade {
     int ScreenHeight;
     int TargetFPS;
   };
-
   class ShadePlayerApp {
     public:
-
-      ShadePlayerApp(int screenWidth, int screenHeight, int fps){
+      ShadePlayerApp(int screenWidth = 1920, int screenHeight = 1080, int fps = 60){
         m_windowConfig = {
           screenWidth,
           screenHeight,
           fps
         };
       }
-
-      ~ShadePlayerApp(){
-
-      }
-
+      ~ShadePlayerApp(){}
       // loads resources and initializes objects needed at start
       void Init(){
  
@@ -45,26 +39,19 @@ namespace shade {
         SetTargetFPS(m_windowConfig.TargetFPS);
 
         // temp
-        m_shader = LoadShader("", "resources/shaders/driveHome.fs");
-        m_iTime = 0.0f;
-        m_iTimeLoc = GetShaderLocation(m_shader, "iTime");
-        m_iResolutionLoc = GetShaderLocation(m_shader, "iResolution");
-        m_iResolution[0] = (float)m_windowConfig.ScreenWidth;
-        m_iResolution[1] = (float)m_windowConfig.ScreenHeight;
-        SetShaderValue(m_shader, m_iResolutionLoc, m_iResolution, UNIFORM_VEC2);
+        Rectangle rec = {0, 0, (float)m_windowConfig.ScreenWidth, (float)m_windowConfig.ScreenHeight};
+        m_layer = ShaderLayer(rec);
+        m_layer.Init("resources/shaders/driveHome.fs");
 
 
-        m_layerStack = LayerStack();
+        //m_layerStack = LayerStack();
       };
-
       // Main app loop
       void Start(){
         while (!WindowShouldClose())
         {
-            // temp update shader params
-            m_iTime += GetFrameTime();
-            SetShaderValue(m_shader, m_iTimeLoc, &m_iTime, UNIFORM_FLOAT);
-
+            float deltaTime = GetFrameTime();
+            
             // do pause ish later
             if (IsKeyPressed(KEY_SPACE)) m_pause = !m_pause;
             //-----------------------------------------------------
@@ -75,33 +62,20 @@ namespace shade {
             ClearBackground(BLANK);
 
             // todo: layerstack rendering
-            
-
-            // temp shader
-            BeginShaderMode(m_shader);
-            DrawRectangle(0, 0, m_windowConfig.ScreenWidth, m_windowConfig.ScreenHeight, WHITE);
-            EndShaderMode();
+            m_layer.Render(deltaTime);
 
             DrawFPS(10, 10);
             EndDrawing();
             ///////////////////////////////
         }
       };
-
     protected:
       bool m_isActive = true;
       int m_zIndex = DEFAULT_ZINDEX;
-
     private:
-      LayerStack m_layerStack;
+      //LayerStack m_layerStack;
+      ShaderLayer m_layer; // temp testing
       WindowConfig m_windowConfig;
-
-      // temp props
-      Shader m_shader;
-      float m_iTime;
-      int m_iTimeLoc;
-      int m_iResolutionLoc;
-      float m_iResolution[2] = {};
       bool m_pause = false;
   };
 }
---
+++ src/ShaderLayer.hpp
@@ -0,0 +1,57 @@
+#ifndef _SHADEPLAYER_INCLUDE_SHADERLAYER_HPP_
+#define _SHADEPLAYER_INCLUDE_SHADERLAYER_HPP_
+#define DEFAULT_ZINDEX 100
+
+#include <raylib.h>
+
+namespace shade {
+
+  /** 
+   * 
+   *  Specialized Layer class for rendering a post-process (screen) shader
+   * 
+   */
+  class ShaderLayer {
+    public:
+      ShaderLayer(){}
+      ShaderLayer(Rectangle dimensions){
+        m_dimensions = dimensions;
+      }
+
+      ~ShaderLayer(){}
+
+      void Init(const char* filePath){
+        m_shader = LoadShader("", filePath);
+
+        // how to handle shader params... perhaps just create one standard 
+        // "interface" or set of params every shader gets passed, whether or not they use them
+        m_iTime = 0.0f;
+        m_iTimeLoc = GetShaderLocation(m_shader, "iTime");
+        m_iResolutionLoc = GetShaderLocation(m_shader, "iResolution");
+        m_iResolution[0] = (float)m_dimensions.width;
+        m_iResolution[1] = (float)m_dimensions.height;
+        SetShaderValue(m_shader, m_iResolutionLoc, m_iResolution, UNIFORM_VEC2);
+      }
+
+      void Render(float deltaTime){
+        m_iTime += deltaTime;
+        SetShaderValue(m_shader, m_iTimeLoc, &m_iTime, UNIFORM_FLOAT);
+        BeginShaderMode(m_shader);
+        DrawRectangle(m_dimensions.x, m_dimensions.y, m_dimensions.width, m_dimensions.height, WHITE);
+        EndShaderMode();
+      }
+
+    protected:
+      bool m_isActive = true;
+      int m_zIndex = DEFAULT_ZINDEX;
+      Rectangle m_dimensions;
+    private:
+      // temp props
+      Shader m_shader;
+      float m_iTime;
+      int m_iTimeLoc;
+      int m_iResolutionLoc;
+      float m_iResolution[2] = {};
+  };
+}
+#endif // _SHADEPLAYER_INCLUDE_SHADERLAYER_HPP_
